02f4948da5c1b3f79cb2b0751d6ca78b2b1f61a0
noobdevsam <mdshoykotislam05@gmail.com>
2026-06-23 21:04:00 +0600

    Add Cycle record for cycle statements with visitor pattern

--- src/main/java/ast/Cycle.java
+++ src/main/java/ast/Cycle.java
@@ -1,4 +1,9 @@
 package ast;
 
-public record Cycle() {
+// whileStmt -> "cycle" "(" expression ")" statement
+public record Cycle(AST.Expr condition, AST.Stmt body) implements AST.Stmt {
+    @Override
+    public <R> R accept(AST.Visitor<R> visitor) {
+        return visitor.visitCycleStmt(this);
+    }
 }
