f2e578cb9b8ab83cbc97326043554a5f1f1d7503
noobdevsam <mdshoykotislam05@gmail.com>
2026-06-23 20:30:39 +0600

    Add TokenType enum for lexer token classification

---
+++ src/main/java/enums/TokenType.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum TokenType {
+    // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    LEFT_BRACKET, RIGHT_BRACKET, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    MANIFEST, FLUX, CYCLE, REVEAL, TRUE, FALSE, NIL,
+
+    EOF
+}
