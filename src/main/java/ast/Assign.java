2872aba685ac670042717da6c58a300cc084c1a9
noobdevsam <mdshoykotislam05@gmail.com>
2026-06-23 20:52:55 +0600

    Add Assign record for assignment expressions with visitor pattern

--- src/main/java/ast/Assign.java
+++ src/main/java/ast/Assign.java
@@ -1,4 +1,11 @@
 package ast;
 
-public record Assign() {
+import lexer.Token;
+
+// assignment -> IDENTIFIER "=" assignment | equality
+public record Assign(Token name, AST.Expr value) implements AST.Expr {
+    @Override
+    public <R> R accept(AST.Visitor<R> visitor) {
+        return visitor.visitAssignExpr(this);
+    }
 }
