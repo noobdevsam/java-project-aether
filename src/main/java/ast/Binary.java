2a06bcbfaf2516b513a16e3cf73953646283e7a5
noobdevsam <mdshoykotislam05@gmail.com>
2026-06-23 20:57:32 +0600

    Add Binary record for binary expressions with visitor pattern

--- src/main/java/ast/Binary.java
+++ src/main/java/ast/Binary.java
@@ -1,4 +1,11 @@
 package ast;
 
-public record Binary() {
+import lexer.Token;
+
+// equality, comparison, term, factor rules
+public record Binary(AST.Expr left, Token operator, AST.Expr right) implements AST.Expr {
+    @Override
+    public <R> R accept(AST.Visitor<R> visitor) {
+        return visitor.visitBinaryExpr(this);
+    }
 }
