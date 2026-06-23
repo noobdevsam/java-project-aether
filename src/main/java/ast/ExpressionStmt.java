43fae845a80c3d071a5dd11c15c14155f320d5a2
noobdevsam <mdshoykotislam05@gmail.com>
2026-06-23 21:02:24 +0600

    Add ExpressionStmt record for expression statements with visitor pattern

--- src/main/java/ast/ExpressionStmt.java
+++ src/main/java/ast/ExpressionStmt.java
@@ -1,4 +1,9 @@
 package ast;
 
-public record ExpressionStmt() {
+// exprStmt -> expression ";"
+public record ExpressionStmt(AST.Expr expression) implements AST.Stmt {
+    @Override
+    public <R> R accept(AST.Visitor<R> visitor) {
+        return visitor.visitExpressionStmt(this);
+    }
 }
