d37d827d2e85dde87b094960b71f8b34338af3e1
noobdevsam <mdshoykotislam05@gmail.com>
2026-06-23 21:03:16 +0600

    Add Flux record for conditional statements with visitor pattern

--- src/main/java/ast/Flux.java
+++ src/main/java/ast/Flux.java
@@ -1,4 +1,9 @@
 package ast;
 
-public record Flux() {
+// ifStmt -> "flux" "(" expression ")" statement ( "else" statement )?
+public record Flux(AST.Expr condition, AST.Stmt thenBranch, AST.Stmt elseBranch) implements AST.Stmt {
+    @Override
+    public <R> R accept(AST.Visitor<R> visitor) {
+        return visitor.visitFluxStmt(this);
+    }
 }
